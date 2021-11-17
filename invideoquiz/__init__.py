a83e0aa3d7ef197a13ad47668783ef42c2e0ded2
Mohammad Ahtasham ul Hassan <ahthassan74@gmail.com>
2021-11-17 15:38:57 +0500

    build: standardize version number placement

--- invideoquiz/__init__.py
+++ invideoquiz/__init__.py
@@ -2,3 +2,5 @@
 Runtime will load the XBlock class from here.
 """
 from .invideoquiz import InVideoQuizXBlock
+
+__version__ = '1.2.0'
--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 """Setup for invideoquiz XBlock."""
 
 import os
+import re
 from setuptools import setup
 from setuptools.command.test import test as TestCommand
 
@@ -66,9 +67,25 @@ class Tox(TestCommand):
         errno = tox.cmdline(args=args)
         sys.exit(errno)
 
+
+def get_version(file_path):
+    """
+    Extract the version string from the file at the given relative path fragments.
+    """
+    filename = os.path.join(os.path.dirname(__file__), file_path)
+    version_file = open(filename).read()
+    version_match = re.search(r"(?m)^__version__ = ['\"]([^'\"]+)['\"]", version_file)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+VERSION = get_version("invideoquiz/__init__.py")
+
+
 setup(
     name='invideoquiz-xblock',
-    version='1.2.0',
+    version=VERSION,
     description='Helper XBlock to locate CAPA problems within videos.',
     license='AGPL v3',
     packages=[
